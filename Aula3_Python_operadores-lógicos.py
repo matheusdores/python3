378dcadc21ec20b14ba5c35964274cacd2c86191
Matheus Dores <105971237+matheusdores@users.noreply.github.com>
2025-01-07 14:23:14 -0300

    Add files via upload

---
+++ Aula3_Python_operadores-lógicos.py
@@ -0,0 +1,14 @@
+print('Menu:\n1 = Escreve Guilherme\n2 = Escreve João\n3 = Escreve Maria')
+
+opcao =  input("Escolha uma opção:")
+
+if opcao == '1':
+    print('Guilherme')
+
+elif opcao == '2':
+    print('João')
+
+elif opcao == '3':
+    print('Maria')
+else:
+     print("Opção inválida")
